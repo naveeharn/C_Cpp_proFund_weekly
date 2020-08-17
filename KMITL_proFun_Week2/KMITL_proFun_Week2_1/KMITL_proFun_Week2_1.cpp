2f167ce041db7dea2f919396beac11a87f0da237
Naveeharn Tehmarn <naveeharntehmarn@MacBook-Pro-khxng-Naveeharn.local>
2020-08-17 13:40:46 +0700

    week2_1

---
+++ KMITL_proFun_Week2/KMITL_proFun_Week2_1/KMITL_proFun_Week2_1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int oddSigma(int n){
+    return n*n;
+}
+
+void print(int n){
+    int t=n,f=1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < t-1; j++)
+        {
+            cout<<" ";
+        }
+        t--;
+        for (int k = 0; k < 2*f-1; k++)
+        {
+            cout<<"*";
+        }
+        f++;
+        cout<<endl;
+        
+    }
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    
+    print(n);
+
+    return 0;
+}
